bd7cb52cfb1c367c39d253a129eb2e08387a79ec
Abhishek lamba <abhishek.lamba@engelvoelkers.com>
2023-07-17 16:43:31 +0200

    first commit

---
+++ Consumer/tests/ConsumerPactTests.cs
@@ -0,0 +1,108 @@
+using System;
+using System.IO;
+using Xunit;
+using Xunit.Abstractions;
+using Consumer;
+using System.Collections.Generic;
+using PactNet;
+using PactNet.Matchers;
+using PactNet.Infrastructure.Outputters;
+using PactNet.Output.Xunit;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net;
+
+namespace tests
+{
+    public class ConsumerPactTests
+    {
+        private IPactBuilderV3 pact;
+
+        public ConsumerPactTests(ITestOutputHelper output)
+        {
+            var Config = new PactConfig
+            {
+                PactDir = Path.Join("..", "..", "..", "..", "..", "pacts"),
+                Outputters = new[] { new XunitOutput(output) },
+                LogLevel = PactLogLevel.Debug
+            };
+
+            pact = Pact.V3("Consumer", "Provider", Config).WithHttpInteractions();
+        }
+
+        [Fact]
+        public async Task ItHandlesInvalidDateParam()
+        {
+            // Arrange
+            var invalidRequestMessage = "validDateTime is not a date or time";
+            pact.UponReceiving("A invalid GET request for Date Validation with invalid date parameter")
+                    .Given("There is data")
+                    .WithRequest(HttpMethod.Get, "/api/provider")
+                    .WithQuery("validDateTime", "lolz")
+                .WillRespond()
+                    .WithStatus(HttpStatusCode.BadRequest)
+                    .WithHeader("Content-Type", "application/json; charset=utf-8")
+                    .WithJsonBody(new { message = invalidRequestMessage });
+
+            // Act
+            await pact.VerifyAsync(async ctx =>
+            {
+                var result = await ConsumerApiClient.ValidateDateTimeUsingProviderApi("lolz", ctx.MockServerUri.ToString());
+                Assert.Equal(HttpStatusCode.BadRequest, result.StatusCode);
+                Assert.Contains(invalidRequestMessage, result.Content.ReadAsStringAsync().GetAwaiter().GetResult());
+            });
+        }
+
+        [Fact]
+        public async Task ItHandlesEmptyDateParam()
+        {
+
+            // Arrange
+            var invalidRequestMessage = "validDateTime is required";
+            pact.UponReceiving("A invalid GET request for Date Validation with empty string date parameter")
+                    .Given("There is data")
+                    .WithRequest(HttpMethod.Get, "/api/provider")
+                    .WithQuery("validDateTime", String.Empty)
+                .WillRespond()
+                    .WithStatus(HttpStatusCode.BadRequest)
+                    .WithHeader("Content-Type", "application/json; charset=utf-8")
+                    .WithJsonBody(new { message = invalidRequestMessage });
+
+            // Act
+            await pact.VerifyAsync(async ctx =>
+            {
+                var result = await ConsumerApiClient.ValidateDateTimeUsingProviderApi(String.Empty, ctx.MockServerUri.ToString());
+                Assert.Equal(HttpStatusCode.BadRequest, result.StatusCode);
+                Assert.Contains(invalidRequestMessage, result.Content.ReadAsStringAsync().GetAwaiter().GetResult());
+            });
+        }
+
+        [Fact]
+        public async Task ItParsesADateCorrectly()
+        {
+            var expectedDateString = "04/05/2018";
+            var expectedDateParsed = DateTime.Parse(expectedDateString).ToString("dd-MM-yyyy HH:mm:ss");
+
+            // Arrange
+            pact.UponReceiving("A valid GET request for Date Validation")
+                    .Given("There is data")
+                    .WithRequest(HttpMethod.Get, "/api/provider")
+                    .WithQuery("validDateTime", expectedDateString)
+                .WillRespond()
+                    .WithStatus(HttpStatusCode.OK)
+                    .WithHeader("Content-Type", "application/json; charset=utf-8")
+                    .WithJsonBody(new
+                    {
+                        test = "NO",
+                        validDateTime = expectedDateParsed
+                    });
+            // Act
+            await pact.VerifyAsync(async ctx =>
+            {
+                var result = await ConsumerApiClient.ValidateDateTimeUsingProviderApi(expectedDateString, ctx.MockServerUri.ToString());
+                Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+                Assert.Contains(expectedDateParsed, result.Content.ReadAsStringAsync().GetAwaiter().GetResult());
+            });
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+ curl -X PUT -H "Content-Type: application/json"   -u pact_workshop:pact_workshop   -d @Consumer-Provider.json  http://localhost:8000/pacts/provider/Provider/consumer/Consumer/version/456780
+
+

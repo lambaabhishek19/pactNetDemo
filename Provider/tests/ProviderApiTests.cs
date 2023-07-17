d2204f0ffb07a55ed88cb7f8abcff22fb945caf2
Abhishek lamba <abhishek.lamba@engelvoelkers.com>
2023-07-17 16:41:36 +0200

    first commit

---
+++ Provider/tests/ProviderApiTests.cs
@@ -0,0 +1,96 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using PactNet.Infrastructure.Outputters;
+using PactNet.Output.Xunit;
+using PactNet.Verifier;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace tests
+{
+    public class ProviderApiTests : IDisposable
+    {
+        private string _providerUri { get; }
+        private string _pactServiceUri { get; }
+        private string _pactBrokerUrl { get; }
+        private IWebHost _webHost { get; }
+        private ITestOutputHelper _outputHelper { get; }
+
+        public ProviderApiTests(ITestOutputHelper output)
+        {
+            _outputHelper = output;
+            _providerUri = "http://localhost:9000";
+            _pactServiceUri = "http://localhost:9001";
+            _pactBrokerUrl = "http://localhost:8000/";
+
+            _webHost = WebHost.CreateDefaultBuilder()
+                .UseUrls(_pactServiceUri)
+                .UseStartup<TestStartup>()
+                .Build();
+
+            _webHost.Start();
+        }
+
+        [Fact]
+        public void EnsureProviderApiHonoursPactWithConsumer()
+        {
+            // Arrange
+            var config = new PactVerifierConfig
+            {
+
+                // NOTE: We default to using a ConsoleOutput,
+                // however xUnit 2 does not capture the console output,
+                // so a custom outputter is required.
+                Outputters = new List<IOutput>
+                                {
+                                    new XunitOutput(_outputHelper)
+                                },
+
+                // Output verbose verification logs to the test output
+                LogLevel = PactNet.PactLogLevel.Debug
+                
+            };
+
+            //Act / Assert
+            IPactVerifier pactVerifier = new PactVerifier(config);
+            var pactFile = new FileInfo(Path.Join("..", "..", "..", "..", "..", "pacts", "Consumer-Provider.json"));
+            pactVerifier.ServiceProvider("Provider", new Uri(_providerUri))
+            .WithPactBrokerSource(new Uri(_pactBrokerUrl),options =>
+    {
+        options.ConsumerVersionSelectors(new ConsumerVersionSelector { Latest = true })
+        .BasicAuthentication("pact_workshop", "pact_workshop")
+        .PublishResults(providerVersion:"1");
+    })
+            .WithProviderStateUrl(new Uri($"{_pactServiceUri}/provider-states"))
+            .Verify();
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _webHost.StopAsync().GetAwaiter().GetResult();
+                    _webHost.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+        }
+        #endregion
+    }
+}
